eeb1abe3db16221f679e4651dedb07cfc06c8d7e
Jasmin1117 <lr.jpascual@mmdc.mcl.edu.ph>
2025-01-24 19:00:12 +0800

    Enhance API with validation and relationships

--- connectly_project/connectly_project/urls.py
+++ connectly_project/connectly_project/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
     path('posts/', include('posts.urls')),
+    
 ]
+
